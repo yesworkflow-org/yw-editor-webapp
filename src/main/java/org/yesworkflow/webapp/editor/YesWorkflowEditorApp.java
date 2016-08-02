fff14e680252d1116e7cbc8736e62418c5d05922
Timothy McPhillips <tmcphillips@absoluteflow.org>
2016-08-01 18:04:57 -0700

    New spring boot web app for visualizing YW graphs code entered into browser.

---
+++ src/main/java/org/yesworkflow/webapp/editor/YesWorkflowEditorApp.java
@@ -0,0 +1,13 @@
+package org.yesworkflow.apps.editor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesWorkflowEditorApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YesWorkflowEditorApp.class, args);
+    }
+
+}
