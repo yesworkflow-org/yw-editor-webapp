537d475460011cadf1ea2fedfed2fdb8ecd506f5
Timothy McPhillips <tmcphillips@absoluteflow.org>
2016-08-01 21:47:03 -0700

    Added option for changing scripting language.

--- src/main/resources/public/main.js
+++ src/main/resources/public/main.js
@@ -1,8 +1,19 @@
 (function() {
 
-  var app = angular.module("YW-Editor-WebApp", []);
+  var app = angular.module("yw-editor-app", []);
 
   var MainController = function($scope, $http) {
+
+    var editor = ace.edit("editor");
+
+    $scope.modeChange = function() {
+      editor.session.setMode("ace/mode/" + $scope.scriptLanguage);
+    }
+
+    editor.setTheme("ace/theme/twilight");
+    editor.session.setMode("ace/mode/python");
+
+    $scope.scriptLanguage = "python";
   };
 
   app.controller("MainController", ["$scope", "$http", MainController]);
