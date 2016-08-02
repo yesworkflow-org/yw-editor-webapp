3a7ba8bf409590d8bb29a2ae9ae47091d53dca8c
Timothy McPhillips <tmcphillips@absoluteflow.org>
2016-08-02 13:51:44 -0700

    Web app now calls graph service.

--- src/main/resources/public/main.js
+++ src/main/resources/public/main.js
@@ -6,14 +6,26 @@
 
     var editor = ace.edit("editor");
 
-    $scope.modeChange = function() {
+    $scope.setMode = function() {
       editor.session.setMode("ace/mode/" + $scope.scriptLanguage);
     }
+    
+    $scope.getGraph = function() {
+      $http.get("http://localhost:8081/api/v1/graph/cache/8834")
+        .then(onGraphComplete);
+    }
+
+    var onGraphComplete = function(response) {
+      $scope.graphId = response.data;
+    };
+
 
     editor.setTheme("ace/theme/twilight");
     editor.session.setMode("ace/mode/python");
+    
 
     $scope.scriptLanguage = "python";
+    $scope.graphId = "foo";
   };
 
   app.controller("MainController", ["$scope", "$http", MainController]);
