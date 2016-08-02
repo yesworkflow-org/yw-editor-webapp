4b7d49785b2a481280b8839df071f54c5ff6893f
Timothy McPhillips <tmcphillips@absoluteflow.org>
2016-08-01 19:10:20 -0700

    Added AngularJS to app.

---
+++ src/main/resources/public/main.js
@@ -0,0 +1,10 @@
+(function() {
+
+  var app = angular.module("YW-Editor-WebApp", []);
+
+  var MainController = function($scope, $http) {
+  };
+
+  app.controller("MainController", ["$scope", "$http", MainController]);
+
+}());
