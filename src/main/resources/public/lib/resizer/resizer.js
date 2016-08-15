42c769a307f9b20c5d17339c9eb9df16267bd5d8
Timothy McPhillips <tmcphillips@absoluteflow.org>
2016-08-15 01:01:29 -0700

    Resizer now handles window resizes.

--- src/main/resources/public/lib/resizer/resizer.js
+++ src/main/resources/public/lib/resizer/resizer.js
@@ -2,57 +2,45 @@ angular.module('mc.resizer', []).directive('resizer', function($document) {
 
 	return function($scope, $element, $attrs) {
 
-		var container = $element.parent();
+		var separator = $element;
+		var container = $(separator).parent()[0];
+		var leftPanel = $attrs.resizerLeft;
+		var rightPanel = $attrs.resizerRight;
+		var separatorWidth = parseInt($attrs.resizerWidth);
 
-		$element.on('mousedown', function(event) {
+		window.addEventListener("resize", onWindowResize);
+
+		function onWindowResize() {
+			if (container.offsetWidth < separator[0].offsetLeft) {
+				moveSliderTo(container.offsetWidth);
+			}
+		};
+
+		$(separator).on('mousedown', function(event) {
 			event.preventDefault();
-
 			$document.on('mousemove', mousemove);
 			$document.on('mouseup', mouseup);
 		});
 
+		function moveSliderTo(x) {
+			$(leftPanel).css({ width: x + 'px' });
+			$(separator).css({ left: x + 'px'});
+			$(rightPanel).css({ left: (x + separatorWidth) + 'px' });
+		}
+
 		function mousemove(event) {
 
-			if ($attrs.resizer == 'vertical') {
+			var x = event.pageX - container.offsetLeft - separatorWidth / 2;
 
-				var x = event.pageX - container[0].offsetLeft - $attrs.resizerWidth / 2;
-
-				if ( x < - $attrs.resizerWidth) {
-					x = - $attrs.resizerWidth;
-				}
-				
-				var containerRight = container[0].offsetWidth;
-
-				if (x > containerRight) {
-					x = containerRight;
-				}
-
-				$element.css({
-					left: x + 'px'
-				});
-
-				$($attrs.resizerLeft).css({
-					width: x + 'px' 
-				});
-				$($attrs.resizerRight).css({
-					left: (x + parseInt($attrs.resizerWidth)) + 'px'
-				});
-
-			} else {
-				// Handle horizontal resizer
-				var y = window.innerHeight - event.pageY;
-
-				$element.css({
-					bottom: y + 'px'
-				});
-
-				$($attrs.resizerTop).css({
-					bottom: (y + parseInt($attrs.resizerHeight)) + 'px'
-				});
-				$($attrs.resizerBottom).css({
-					height: y + 'px'
-				});
+			if ( x < -separatorWidth) {
+				x = -separatorWidth;
 			}
+
+			if (x > container.offsetWidth) {
+				x = container.offsetWidth;
+			}
+
+			moveSliderTo(x);
 
 			// $scope.$apply();
 		}
